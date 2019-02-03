06b86db34d5010f6412a74662ec60c781423e1f2
korbei <tamas.korbei@gmail.com>
2019-02-03 14:03:09 +0100

    commit

--- src/main/java/com/korbei/rs/jwt/JwtDynamicFeature.java
+++ src/main/java/com/korbei/rs/jwt/JwtDynamicFeature.java
@@ -71,39 +71,42 @@ public class JwtDynamicFeature implements DynamicFeature {
         @Override
         public void filter(ContainerRequestContext ctx) throws IOException {
             final String authorizationHeader = ctx.getHeaderString(HttpHeaders.AUTHORIZATION);
-            if (authorizationHeader != null && authorizationHeader.startsWith(Const.JWT_AUTH_SCHEME)) {
-                try {
-                    final String token = authorizationHeader.substring(7);
-                    final DecodedJWT decodedJWT = Token.verify(token);
-                    final boolean isSecure = ctx.getSecurityContext().isSecure();
 
-                    ctx.setSecurityContext(new SecurityContext() {
-                        @Override
-                        public Principal getUserPrincipal() {
-                            return decodedJWT::getSubject;
-                        }
+            if(authorizationHeader == null || !authorizationHeader.startsWith(Const.JWT_AUTH_SCHEME)) {
+                throw new NotAuthorizedException(Const.JWT_AUTH_SCHEME);
+            }
 
-                        @Override
-                        public boolean isUserInRole(String role) {
-                            return decodedJWT
-                                    .getClaim(Const.CLAIM_ROLES)
-                                    .asList(String.class)
-                                    .contains(role);
-                        }
+            try {
+                final String token = authorizationHeader.substring(7);
+                final DecodedJWT decodedJWT = Token.verify(token);
+                final boolean isSecure = ctx.getSecurityContext().isSecure();
 
-                        @Override
-                        public boolean isSecure() {
-                            return isSecure;
-                        }
+                ctx.setSecurityContext(new SecurityContext() {
+                    @Override
+                    public Principal getUserPrincipal() {
+                        return decodedJWT::getSubject;
+                    }
 
-                        @Override
-                        public String getAuthenticationScheme() {
-                            return Const.JWT_AUTH_SCHEME;
-                        }
-                    });
-                } catch (JWTVerificationException e) {
-                    throw new NotAuthorizedException(Const.JWT_AUTH_SCHEME);
-                }
+                    @Override
+                    public boolean isUserInRole(String role) {
+                        return decodedJWT
+                                .getClaim(Const.CLAIM_ROLES)
+                                .asList(String.class)
+                                .contains(role);
+                    }
+
+                    @Override
+                    public boolean isSecure() {
+                        return isSecure;
+                    }
+
+                    @Override
+                    public String getAuthenticationScheme() {
+                        return Const.JWT_AUTH_SCHEME;
+                    }
+                });
+            } catch (JWTVerificationException e) {
+                throw new NotAuthorizedException(Const.JWT_AUTH_SCHEME);
             }
         }
     }
